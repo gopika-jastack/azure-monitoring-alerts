40d3db7352a816f78bc0cbc8a6a45eb3e73e3092
gopika-jastack <gopikashree2000@gmail.com>
2026-06-24 17:37:26 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure Region"
+  default     = "UK South"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  default     = "rg-monitoring-demo"
+}
