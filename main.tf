bf74f3817f5ee044782dc813426be354008c3ab8
gopika-jastack <gopikashree2000@gmail.com>
2026-06-25 12:37:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,3 +35,39 @@ resource "azurerm_monitor_action_group" "ag" {
     email_address = "admin@example.com"
   }
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "log_alert" {
+  name                = "log-analytics-alert"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+  severity             = 2
+
+  scopes = [
+    azurerm_log_analytics_workspace.law.id
+  ]
+
+  criteria {
+    query = <<-QUERY
+      AzureActivity
+      | where Level == "Error"
+    QUERY
+
+    operator                = "GreaterThan"
+    threshold               = 0
+    time_aggregation_method = "Count"
+  }
+
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.ag.id
+    ]
+  }
+}
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ag.id
+  }
+}
